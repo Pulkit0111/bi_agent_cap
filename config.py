99cd46895cc38c4ef2bb627eea33c0d7939840ef
Pulkit0111 <ptyagi1551@gmail.com>
2025-06-27 15:12:35 +0530

    resolved the db connection and flag issue

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.utilities import SQLDatabase
 from sqlalchemy import inspect
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -10,8 +11,8 @@ openai_key = os.getenv("OPENAI_API_KEY")
 if not openai_key:
     raise ValueError("OPENAI_API_KEY not found in .env file!")
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
 
 ENABLE_SERVER_SQL_EXEC = os.getenv("ENABLE_SERVER_SQL_EXEC", "false").lower() == "true"
 
@@ -23,7 +24,14 @@ if ENABLE_SERVER_SQL_EXEC:
     sql_driver = os.getenv("SQL_DRIVER")
     db_path = os.getenv("DB_PATH")
     
-    DB_URI = f"mssql+pyodbc://{sql_user}:{sql_pass}@{sql_server}/{sql_db}?driver={sql_driver}&TrustServerCertificate=yes"
+    params = quote_plus(
+        f"DRIVER={sql_driver};"
+        f"SERVER={sql_server};"
+        f"DATABASE={sql_db};"
+        f"UID={sql_user};PWD={sql_pass};"
+        f"Encrypt=yes;TrustServerCertificate=yes;"
+    )
+    DB_URI = f"mssql+pyodbc:///?odbc_connect={params}"
     # DB_URI = db_path
     
     if not DB_URI:
